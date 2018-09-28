049e2cf3bf0e7ad11ca77b247241d8fd3c302294
Philippe Duong <philippe@heyday.ai>
2018-09-28 14:43:50 -0400

    npm install

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,13 +1,13 @@
-import { NgModule } from "@angular/core";
-import { RouterModule, Routes } from "@angular/router";
-import { TestComponent } from "./test/test.component";
-import { InputExampleComponent } from "./input-example/input-example.component"
-import { FixedComponent } from "./fixed/fixed.component";
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { TestComponent } from './test/test.component';
+import { InputExampleComponent } from './input-example/input-example.component';
+import { FixedComponent } from './fixed/fixed.component';
 
 const routes: Routes = [
-  { path: "users", component: TestComponent },
-  { path: "input-example", component: InputExampleComponent },
-  { path: "fixed", component: FixedComponent },
+  { path: 'users', component: TestComponent },
+  { path: 'input-example', component: InputExampleComponent },
+  { path: 'fixed', component: FixedComponent },
 ];
 
 @NgModule({
@@ -15,5 +15,5 @@ const routes: Routes = [
   exports: [RouterModule]
 })
 export class AppRoutingModule {
-  public name = "Jen";
+  public name = 'Jen';
 }
